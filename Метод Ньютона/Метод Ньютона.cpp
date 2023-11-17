eda31ad4286d98d5af4af72cad0553fb338be946
Julia <julieshaban@yandex.ru>
2023-11-18 01:00:34 +0300

    checking the Jacobian calculation

--- Метод Ньютона/Метод Ньютона.cpp
+++ Метод Ньютона/Метод Ньютона.cpp
@@ -1,23 +1,26 @@
-﻿#include "newtonMethod.h";
+﻿#include "newtonMethod.h"
+#include "Gauss.h"
 
 void main() 
 {
 
-	int n;
-	cout << "Number of equations: ";
-	cin >> n;
-
+	int n=2;
 	double x0 = 1, y0 = 0;
 	cout << setw(15) << "Iteration" << setw(15) << "Delta 1" << setw(15) << "Delta 2\n";
-	newtonMethod(x0, y0, n);
+	newtonMethod(x0, y0, n, Jac(x0,y0,n));
 	vector<double> M{0.01, 0.05, 0.1};
 	for (int i = 0; i < M.size(); i++)
 	{
-		cout << endl<<endl;
-		cout << "\nM[" << i + 1 << "] = " << M[i];
-		cout << endl << endl;
-		cout << endl<< setw(15) << "Iteration" << setw(15) << "Delta 1" << setw(15) << "Delta 2\n";
-		newtonMethod(x0, y0, n, M[i]);
+		cout << endl;
+			cout << "\nM[" << i + 1 << "] = " << M[i];
+		if (x0 == 0 || y0 == 0)
+			cout << "\nJac cannot be counted";
+		else
+		{
+			
+			cout << endl << setw(15) << "Iteration" << setw(15) << "Delta 1" << setw(15) << "Delta 2\n";
+			newtonMethod(x0, y0, n, Jac(x0, y0, n, M[i]));
+		}
 	}
 
 
