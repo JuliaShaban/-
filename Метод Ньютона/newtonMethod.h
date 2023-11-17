d34113bfaff2189b17b79958c8ca846230926823
Julia <julieshaban@yandex.ru>
2023-11-18 00:57:49 +0300

    add function

--- Метод Ньютона/newtonMethod.h
+++ Метод Ньютона/newtonMethod.h
@@ -5,6 +5,7 @@
 
 using namespace std;
 
-void newtonMethod(double x1, double x2, int n);
-void newtonMethod(double x1, double x2, int n, double M);
+void newtonMethod(double x1, double x2, int n, vector<vector<double>> Jac);
+vector<vector<double>> Jac(double x1, double x2, int n, double M);
+vector<vector<double>> Jac(double x1, double x2, int n);
 
